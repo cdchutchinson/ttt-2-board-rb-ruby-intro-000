84d32ec779fe4f5bd6858b8c9803a8c7690bfaec
Crystal <cdchutchinson@gmail.com>
2020-03-02 02:31:40 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,1 @@
-# Define the variable board below.
-row 1= ["0", "1", "2"]
-row 2= ["3", "4", "5"]
-row 3= ["6", "7", "8"]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]# Define the variable board below.
